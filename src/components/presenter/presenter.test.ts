39de6a4a6794093381de8d750fec4d9e713882b9
Maxim Dejnew <red41tiger@mail.ru>
2022-01-20 14:09:20 +0700

    Experimenting with jasmine tests

--- src/components/presenter/presenter.test.ts
+++ src/components/presenter/presenter.test.ts
@@ -1,14 +1,27 @@
 /* eslint-disable */
 import Presenter from './presenter';
 
-describe('Math test', () => {
-  it('A2', () => {
-    expect( Presenter.testFunc(2) ).toBe(4);
+describe('PRESENTER TESTS', () => {
+  describe('check', () => {
+    const presenter = new Presenter('body');
+
+    it('A2', () => {
+      expect( presenter.testFunc(2) ).toBe(4);
+    });
+    it('B4', () => {
+      expect( presenter.testFunc(4) ).toBe(8);
+    });
+    it('C16', () => {
+      expect( presenter.testFunc(8) ).toBe(16);
+    });
   });
-  it('B4', () => {
-    expect( Presenter.testFunc(4) ).toBe(8);
-  });
-  it('C16', () => {
-    expect( Presenter.testFunc(8) ).toBe(16);
+
+  describe('render', () => {
+    const presenter = new Presenter('body');
+    it('rendering'), () => {
+      const wrapRef = 'body';
+
+      expect( presenter.defineElementByRef(wrapRef) ).toBe( document.body );
+    }
   });
 });
--- src/components/presenter/presenter.ts
+++ src/components/presenter/presenter.ts
@@ -1,14 +1,26 @@
-type IWrap = HTMLElement | string;
+type IWrapRef = HTMLElement | string;
+type IElement = HTMLElement | null;
 
 class Presenter {
-  private wrap: IWrap;
+  private wrapElement:IElement;
 
-  constructor(wrap: IWrap) {
-    this.wrap = wrap;
+  constructor(wrapRef:IWrapRef) {
+    this.wrapElement = this.defineElementByRef(wrapRef);
+    this.render(this.wrapElement);
   }
 
-  static testFunc(x:number):number {
+  testFunc(x:number):number {
     return x * 2;
+  }
+
+  defineElementByRef(wrapRef:IWrapRef):HTMLElement {
+    const caseWrapIsSelector = typeof wrapRef === 'string';
+    return caseWrapIsSelector ? document.querySelector(wrapRef) || document.body : wrapRef;
+  }
+
+  private render(wrapElement:HTMLElement) {
+    const insertableElement = document.createElement('<span>HelloWorld!</span>');
+    wrapElement.insertAdjacentElement('afterbegin', insertableElement);
   }
 }
 
