925b150c38a69883c3c1069d7dfb2b09ebcb7ade
Maxim Dejnew <red41tiger@mail.ru>
2022-02-06 15:25:43 +0700

    Minor codestyle fixes

--- src/components/presenter/presenter.ts
+++ src/components/presenter/presenter.ts
@@ -1,32 +1,6 @@
-type IWrapRef = Element | string;
-
 class Presenter {
-  private readonly wrapElement:Element;
-
-  constructor(wrapRef:IWrapRef) {
-    this.wrapElement = Presenter.defineElementByRef(wrapRef);
-    this.render();
-  }
-
-  static defineElementByRef(wrapRef:IWrapRef):Element {
-    const caseWrapRefSelector = typeof wrapRef === 'string';
-
-    if (caseWrapRefSelector) {
-      const caseWrapRefExists = document.querySelector(wrapRef);
-
-      if (caseWrapRefExists) {
-        return caseWrapRefExists;
-      }
-      throw new Error('Incorrect wrap element reference!');
-    } else {
-      return wrapRef;
-    }
-  }
-
-  private render() {
-    const { wrapElement } = this;
-    const insertableElement = document.createElement('div');
-    wrapElement.insertAdjacentElement('afterbegin', insertableElement);
+  constructor() {
+    
   }
 }
 
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,24 +1,15 @@
 //import jQuery from 'jquery';
-import DEFAULTS from './defaults';
+import {
+  ISliderFunction,
+  ISliderOptionsList,
+  ISliderOptions,
+} from './types/plugin';
 
 import Model from './components/model/model';
 import View from './components/view/view';
 import Presenter from './components/presenter/presenter';
 
-interface ISliderOptionsList {
-  outputSelector?: string,
-  vertical?: boolean,
-  step?: number,
-  scale?: boolean,
-};
-
-interface ISliderOptions {
-  options: ISliderOptionsList,
-};
-
-interface ISliderFunction {
-  (this: JQuery, options: ISliderOptionsList): JQuery,
-};
+import DEFAULTS from './defaults';
 
 declare global {
   interface JQuery {
@@ -26,30 +17,27 @@ declare global {
   }
 };
 
-// Define the plugin function on the jQuery extension point.
-// Note: Function and global default options must be combined as long as the options are mandatory.
 (function($) {
+  const OPTIONS = {
+    options: DEFAULTS,
+  };
+
   const INIT = function initSlider(this: JQuery, options: ISliderOptionsList): JQuery {
-    // Merge the global options with the options given as argument.
     options = $.extend({}, $.fn.slider.options, options);
 
-    // Check if required options are missing.
     if (!options.outputSelector) {
       console.error(
         'Example plugin options are missing required parameter "outputSelector": ',
         JSON.stringify(options)
       );
+      return this;
     }
-    // Do what the plugin should do. Here we create an instance of the separate service
+    const model = new Model();
+    const view = new View();
+    const presenter = new Presenter();
     
-    // Return the jQuery object for chaining.
     return this;
   };
 
-  $.fn.slider = Object.assign<ISliderFunction, ISliderOptions>(
-    INIT, 
-    {
-      options: DEFAULTS
-    }
-  );
+  $.fn.slider = Object.assign<ISliderFunction, ISliderOptions>(INIT, OPTIONS);
 })(jQuery);
