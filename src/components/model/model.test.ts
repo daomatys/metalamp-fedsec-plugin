408044f95aa53a96eabe3d630e18b6ddd47cb564
Maxim Dejnew <overmaxcoast@yandex.ru>
2023-07-15 14:20:21 +0700

    [~] Model: Test: Added new test

--- src/components/model/model.test.ts
+++ src/components/model/model.test.ts
@@ -5,4 +5,5 @@ test('Math test', () => {
   expect( Model.testFunc(2) ).toBe(4);
   expect( Model.testFunc(4) ).toBe(8);
   expect( Model.testFunc(8) ).toBe(16);
+  expect( Model.testFunc(10) ).toBe(20);
 });
